9f82d223d37abc66513d596d5bab370f2c7acaf9
Harrison Ajisogun <harrisonajisogun@harrisons-mbp.mynetworksettings.com>
2024-02-13 18:00:33 -0500

    added code

--- main.py
+++ main.py
@@ -1,37 +1,71 @@
-import sys 
+shift  = 10
+
+message = "helloworld"
+encrypted_message = ""
+
+for  x in message:
+
+    # check if character is uppercase letter:
+     if x.isupper():
+
+        # find the position in 0-25
+        x_unicode = ord(x)
+
+        x_index = ord(x) - ord("A")
+
+        # we need a negative value on the shift
+        x_index = (x_index + shift) % 26 
+
+        # make a new character
+
+        new_unicode =  x_index + ord("A")
+
+        new_character = chr( new_unicode)
+
+        # append to encrypt word
+        encrypted_message += new_character
+
+     else:
+        # if the character is not lower case we should the letter as it is
+        encrypted_message += x
+
+shift  = 10
+
+message = "rovvy gybvn"
+decrypted_message = ""
+
+for  x in message:
+
+    # check if character is uppercase letter:
+     if x.isupper():
+
+        # find the position in 0-25
+        x_unicode = ord(x)
+
+        x_index = ord(x) - ord("A")
+
+        # we need a negative value on the shift
+        x_index = (x_index - shift) % 26 
+
+        # make a new character
+
+        new_unicode =  x_index + ord("A")
+
+        new_character = chr( new_unicode)
+
+        # append to decrypt word
+        decrypted_message += new_character
+
+     else:
+        # if the character is not lower case we should the letter as it is
+        decrypted_message += x
+    
+
+print("Your encrypted word is ",encrypted_message)
+print("Your Decrypted word is ",decrypted_message)
 
 
-def encrypt(message, k):
-    encrypted_message = ""
-    for x in message.lower():
-        if x.isalpha(): # Check if the character is an alphabet letter
-            # If the character is lowercase, set shift to ord('a') - 97; if it's uppercase, set shift to ord('A') - 65
-            shift = ord('A') if x.isupper() else ord('a')  
-             # Encrypt the character using a Caesar cipher, transforming the letter into a secret code (number) and then back to a letter
-            encrypted_x = chr((ord(x) - shift + k) % 26 + shift) 
-            encrypted_message += encrypted_x
-        else:
-            encrypted_message += x
-    return encrypted_message
 
 
-def decrypt(message, k):
-    return encrypt(message, -k)
 
-
-if __name__ == "__main__":
-    # take in first arg as word
-    message = sys.argv[1]
-    # take in second arg as int key
-    key = int(sys.argv[2])
-
-    # encrypt your word
-    encrypt_message = encrypt(message, key)
-
-    # decrypt your encrypted word
-    decrypted_message = decrypt(encrypt_message, key)
-
-    print("Your encrypted word is", encrypt_message)
-    print("Your decrypted word is", decrypted_message)
-
-
+    
